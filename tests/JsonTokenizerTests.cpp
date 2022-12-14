077613e77a6978c9cbd8a6b9c4aa0bb6900a3be6
michel <michel.dierckx@student.uantwerpen.be>
2022-12-14 23:46:46 +0100

    added tests for json tokenizer

---
+++ tests/JsonTokenizerTests.cpp
@@ -0,0 +1,26 @@
+//
+// Created by michel on 14.12.22.
+//
+
+#include "../lib/doctest/doctest.h"
+#include <fstream>
+#include <sstream>
+
+#include "../src/objects/JsonTokenizer.h"
+
+TEST_SUITE("JsonTokenizerTests") {
+    TEST_CASE("[JsonTokenizerTests] test0") {
+
+        const std::ifstream expectedFile("../tests/expected/expected-json_tokenizer1.txt");
+        std::stringstream expected;
+        expected << expectedFile.rdbuf();
+
+        std::stringstream actual("");
+
+        JsonTokenizer j;
+        j.tokenize("../tests/input/input-json_tokenizer1.json");
+        j.printTokens(actual);
+
+        CHECK_EQ(expected.str(), actual.str());
+    }
+}
