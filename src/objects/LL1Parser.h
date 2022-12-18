8dee076da6bbaf36c6deafa74041d1ccc46d0f19
Axelmans <axel.deleeuw@icloud.com>
2022-12-18 16:44:22 +0100

    added LL parser class

---
+++ src/objects/LL1Parser.h
@@ -0,0 +1,23 @@
+//
+// Created by axeld on 18/12/2022.
+//
+
+#ifndef MB_GROEP_22_23_LL1PARSER_H
+#define MB_GROEP_22_23_LL1PARSER_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+#include "CFG.h"
+
+class LL1Parser {
+public:
+    explicit LL1Parser(const CFG&);
+    bool accepts(const std::string&);
+private:
+    CFG grammar;
+    bool match(std::string&, std::string&);
+};
+
+#endif //MB_GROEP_22_23_LL1PARSER_H
