02a048278fc837f4b41e200ce6849494b4d9c465
Slendy <siebe.wijnants@hotmail.com>
2022-12-27 14:13:36 +0100

    EML grammar update!

---
+++ src/objects/EMLGrammarGenerator.cpp
@@ -0,0 +1,115 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include "EMLGrammarGenerator.h"
+
+void EMLGrammarGenerator::generate() {
+    // fixed path
+    std::ofstream file("../res/eml_grammar.json");
+
+    nlohmann::json j;
+    generateStart(j);
+    generateVariables(j);
+    generateTerminals(j);
+    generateProductions(j);
+
+    file << std::setw(4) << j;
+    file.close();
+}
+
+void EMLGrammarGenerator::generateStart(nlohmann::json& j) { j["Start"] = "eml"; }
+
+void EMLGrammarGenerator::generateVariables(nlohmann::json& j) {
+    j["Variables"] = {"eml",    "value",   "object",     "members",   "member", "array",   "elements",
+                      "element", "OTag", "CTag", "string",  "characters", "character", "number", "integer", "digits",
+                      "digit",   "onenine", "fraction",   "exponent",  "sign",   "ws"};
+}
+
+void EMLGrammarGenerator::generateTerminals(nlohmann::json& j) {
+    j["Terminals"] = {"<", ">", "/", "-", ".", "+", " ", "\n", "\t"};
+    for (char ch = '0'; ch <= '9'; ch++) {
+        j["Terminals"].emplace_back(std::string(1, ch));
+    }
+    for (char ch = 'A'; ch <= 'Z'; ch++) {
+        j["Terminals"].emplace_back(std::string(1, ch));
+    }
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+        j["Terminals"].emplace_back(std::string(1, ch));
+    }
+    j["Terminals"].emplace_back("?");
+    j["Terminals"].emplace_back("!");
+    j["Terminals"].emplace_back("(");
+    j["Terminals"].emplace_back(")");
+}
+
+void EMLGrammarGenerator::generateProductions(nlohmann::json& j) {
+    addProduction(j, "eml", {"<", "r", "o", "o", "t", ">", "element", "<", "/", "r", "o", "o", "t", ">"});
+    addProductions(j, "value",
+                   {{"object"},
+                    {"array"},
+                    {"string"},
+                    {"number"},
+                    {"t", "r", "u", "e"},
+                    {"f", "a", "l", "s", "e"},
+                    {"n", "u", "l", "l"}});
+    addProductions(j, "object", {{"OTag", "ws", "CTag"}, {"OTag", "members", "CTag"}});
+    addProductions(j, "members", {{"member"}, {"member", "members"}});
+    addProduction(j, "member", {"element"});
+    addProductions(j, "array", {{"ws"}, {"elements"}});
+    addProductions(j, "elements", {{"element"}, {"element", "elements"}});
+    addProduction(j, "element", {"ws", "value", "ws"});
+    addProduction(j, "OTag", {"<", "string", ">"});
+    addProduction(j, "CTag", {"<", "/", "string", ">"});
+    addProduction(j, "string", {"characters"});
+    addProductions(j, "characters", {{}, {"character", "characters"}});
+    addProductionsCharacter(j);
+    addProduction(j, "number", {"integer", "fraction", "exponent"});
+    addProductions(j, "integer", {{"digit"}, {"onenine", "digits"}, {"-", "digit"}, {"-", "onenine", "digits"}});
+    addProductions(j, "digits", {{"digit"}, {"digit", "digits"}});
+    addProductions(j, "digit", {{"0"}, {"onenine"}});
+    addProductionsOneNine(j);
+    addProductions(j, "fraction", {{}, {".", "digits"}});
+    addProductions(j, "exponent", {{}, {"E", "sign", "digits"}, {"e", "sign", "digits"}});
+    addProductions(j, "sign", {{}, {"+"}, {"-"}});
+
+    // addProductions(j, "ws", {{}, {" "}, {"\n"}, {"\t"}});
+
+    // CHANGED PRODUCTION
+    addProductions(j, "ws", {{}, {" ", "ws"}, {"\n", "ws"}, {"\t", "ws"}});
+}
+
+void EMLGrammarGenerator::addProduction(nlohmann::json& j, const std::string& head,
+                                         const std::vector<std::string>& body) {
+    nlohmann::json object;
+    object["head"] = head;
+    object["body"] = body;
+    j["Productions"].emplace_back(object);
+}
+void EMLGrammarGenerator::addProductions(nlohmann::json& j, const std::string& head,
+                                          const std::vector<std::vector<std::string>>& bodies) {
+    for (auto& body : bodies) {
+        addProduction(j, head, body);
+    }
+}
+void EMLGrammarGenerator::addProductionsCharacter(nlohmann::json& j) {
+    for (char ch = '0'; ch <= '9'; ch++) {
+        addProduction(j, "character", {std::string(1, ch)});
+        ;
+    }
+    for (char ch = 'A'; ch <= 'Z'; ch++) {
+        addProduction(j, "character", {std::string(1, ch)});
+        ;
+    }
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+        addProduction(j, "character", {std::string(1, ch)});
+        ;
+    }
+    addProductions(j, "character", {{"?"}, {"!"}, {"("}, {")"}, {"."}, {"+"}, {"-"}, {" "}, {","}, {":"}});
+}
+void EMLGrammarGenerator::addProductionsOneNine(nlohmann::json& j) {
+    for (char ch = '1'; ch <= '9'; ch++) {
+        addProduction(j, "onenine", {std::string(1, ch)});
+        ;
+    }
+}
---
+++ src/objects/EMLGrammarGenerator.h
@@ -0,0 +1,29 @@
+#ifndef MB_GROEP_22_23_EMLGRAMMARGENERATOR_H
+#define MB_GROEP_22_23_EMLGRAMMARGENERATOR_H
+
+#include "../../lib/nlohmann-json/json.hpp"
+#include <string>
+
+class EMLGrammarGenerator {
+public:
+    static void generate(); // generate the eml grammar in res/eml_grammar.json, based on
+                            // the simplified json form specified in "https://www.json.org/json-en.html"
+                            // simplifications:
+                            // a character can only be: a . z, A . Z, 0 . 9, '.', '?', '!', '+', '-', '
+                            // '
+                            // a ws can not be: \r
+private:
+    static void generateStart(nlohmann::json& j);   // generate start symbol
+    static void generateVariables(nlohmann::json& j);   // generate all variables for json
+    static void generateTerminals(nlohmann::json& j);   // generate all terminals for json
+    static void generateProductions(nlohmann::json& j); // generate all productions for json
+    static void addProduction(nlohmann::json& j, const std::string& head,
+                              const std::vector<std::string>& body); // add one production to json j
+    static void addProductions(
+        nlohmann::json& j, const std::string& head,
+        const std::vector<std::vector<std::string>>& bodies); // add multiple productions to json j
+    static void addProductionsCharacter(nlohmann::json& j);   // add all productions for "character" to json j
+    static void addProductionsOneNine(nlohmann::json& j);     // add all productions for "onenine" to json j
+};
+
+#endif // MB_GROEP_22_23_EMLGRAMMARGENERATOR_H
