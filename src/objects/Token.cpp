1836636e43cbefa4192623241536813a79592e2e
michel <michel.dierckx@student.uantwerpen.be>
2022-12-28 16:03:04 +0100

    changed print function token

--- src/objects/Token.cpp
+++ src/objects/Token.cpp
@@ -1,6 +1,7 @@
 #include "Token.h"
 #include <iostream>
 #include <utility>
+#include <iomanip>
 
 position::position(unsigned int l, unsigned int c) : line{l}, column{c} {}
 token::token(std::string cont, unsigned int l, unsigned int c) : content{std::move(cont)}, pos{l, c}, type("UNKNOWN") {}
@@ -10,7 +11,10 @@ token::token(std::string cont, const position& p, std::string t)
 token::token(std::string cont, unsigned int l, unsigned int c,  std::string t) : content{std::move(cont)}, pos{l, c}, type(std::move(t)) {}
 
 void token::print(std::ostream& out = std::cout) const {
-    out << "(" << pos.line << ", " << pos.column << "): " << content << " [" << type << "]" << std::endl;
+    out << std::left
+    << std::setw(16) << "(" + std::to_string(pos.line) + ", " +  std::to_string(pos.column) + "):"
+    << std::setw(40) << content
+    << "[" + type + "]" << std::endl;
 }
 void token::reset() {
     content = "";
